ad562217716f6261a859e67a371603189cfe571c
walisonls <gwalisonls@gmail.com>
2022-06-13 16:46:39 -0300

    Enter

--- script.js
+++ script.js
@@ -2,7 +2,3 @@ var rotateX = 329;
 var rotateY = 59;
 
 document.querySelector(".cube").style = `transform: rotateX(${rotateX}deg) rotateY(${rotateY}deg);`;
-
-function degTop(){
-    rotateX = rotateX + 3
-}
