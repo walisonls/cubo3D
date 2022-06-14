7f2fe392577179b153252cb8ce41379fc6bbe24d
walisonls <gwalisonls@gmail.com>
2022-06-14 08:21:18 -0300

    Enter

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-var rotateX = 329;
-var rotateY = 59;
+let rotateX = 329;
+let rotateY = 59;
 
-document.querySelector(".cube").style = `transform: rotateX(${rotateX}deg) rotateY(${rotateY}deg);`;
+
+function adicionarX(){
+    rotateX = rotateX + 3
+
+    document.querySelector(".cube").style = `transform: rotateX(${rotateX}deg) rotateY(${rotateY}deg);`;
+}
+
+function removerX(){
+    rotateX = rotateX - 3
+
+    document.querySelector(".cube").style = `transform: rotateX(${rotateX}deg) rotateY(${rotateY}deg);`;
+}
+
+function adicionarY(){
+    rotateY = rotateY + 3
+
+    document.querySelector(".cube").style = `transform: rotateX(${rotateX}deg) rotateY(${rotateY}deg);`;
+}
+
+function removerY(){
+    rotateY = rotateY - 3
+
+    document.querySelector(".cube").style = `transform: rotateX(${rotateX}deg) rotateY(${rotateY}deg);`;
+}
+
+adicionarX()
